86fb8293b49ad22e5e3933afef795cf88633e4e3
Arif Imeri <arifimeri.11@gmail.com>
2025-01-21 14:48:18 +0100

    added some new folders and files

--- app/src/main/java/com/example/projekti2/MainActivity.kt
+++ app/src/main/java/com/example/projekti2/MainActivity.kt
@@ -11,24 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.projekti2.ui.theme.Projekti2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Projekti2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -41,7 +34,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Projekti2Theme {
-        Greeting("Android")
-    }
+
 }
